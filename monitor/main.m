69eaecd5352953cb210fce8792d4d245563f8983
Aaron Attig <aaronattig@andys-macbook-pro.local>
2016-04-02 09:19:59 -0700

    Initial Commit

---
+++ monitor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  monitor
+//
+//  Created by Aaron Attig on 4/2/16.
+//  Copyright © 2016 Aaron Attig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
